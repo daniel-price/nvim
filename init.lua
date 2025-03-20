67f6c544c5ec9ac0c687727df6a291995ca1a248
Daniel Price <64694785+daniel-price@users.noreply.github.com>
2025-03-20 15:17:59 +0000

    Remove conflicting keymap

--- init.lua
+++ init.lua
@@ -855,13 +855,6 @@ plugin({
             end,
           })
         end
-
-        -- Create a keymap to toggle inlay hints, if the language server you are using supports them
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-          map("<leader>th", function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-          end, "[t]oggle inlay [h]ints")
-        end
       end,
     })
 
