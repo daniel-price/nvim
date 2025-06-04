905842790548114de8c9c833a6f8476cb25063da
Daniel Price <64694785+daniel-price@users.noreply.github.com>
2025-06-04 17:23:19 +0100

    remove autosort imports on save

--- init.lua
+++ init.lua
@@ -179,11 +179,6 @@ vim.api.nvim_create_autocmd("FileType", {
   desc = "Quickfix tweaks",
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.ts*",
-  command = ":TSToolsOrganizeImports sync",
-})
-
 ---------------------
 ------ PLUGINS ------
 ---------------------
